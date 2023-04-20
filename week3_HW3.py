3cef0c2d511b5da0fb45ab828cde43a962f2b88b
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-04-19 22:42:08 -0400

    Create week3_HW3.py

---
+++ week3_HW3.py
@@ -0,0 +1,12 @@
+#  Exercise 1: Create a method prints an image of your pokemon
+# Display on image in Jupyter notebook
+
+from Ipython.display import image
+
+display_image()
+
+
+# Exercise 2: Create a method that evolves your pokemon. If your pokemon can't evolve any further print a message that says "<name of pokemon>" can't evolve."
+
+# Exercise 3: Create a Move_Tutor Class that will allow the POkemon Class to inherit a move list. For an added bonus you can make sure that if 
+# a pokemon has 4 moves the user can choose one of them to replace with a new move.
