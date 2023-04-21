2156f4afa13e101cd5460d515c7d54b9e1171d56
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-04-21 10:33:18 -0400

    Update week3_HW4.py

--- week3_HW4.py
+++ week3_HW4.py
@@ -11,7 +11,7 @@ class Test_Cart(unittest.TestCase):
         quantity = input(f"How many {item} would you like to add?: ")
         self.assertEqual(len(Cart.grocery_dict)) == 1
         self.Cart.grocery_dict['bread']['quantity'] == 2
-        self.Cart.grocery_dict['bread']['price'] == 2.5
+        self.Cart.grocery_dict['eggs']['price'] == 2.5
         expected_output = (result, f"You have {item} in the cart.")
         self.add()
 
@@ -21,6 +21,7 @@ class Test_Cart(unittest.TestCase):
         self.Cart.add()
         self.Cart.remove()
         self.assertLess(len(Cart.grocery_dict)) == 0
+        self.Cart.grocery_dict['apples']['quantity'] == 3
         expected_output = (quantity, f"you have remove {item_to_remove} in the cart")
 
     def test_show(self):
