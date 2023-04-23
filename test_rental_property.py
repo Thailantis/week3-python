5def17c01eaafb914aff37f4eb3e91d2654609b7
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-04-23 17:21:44 -0400

    Update test_rental_property.py

--- test_rental_property.py
+++ test_rental_property.py
@@ -1,5 +1,6 @@
 import unittest
 
+
 class Test_Rental_Property(unittest.TestCase):
     def test_calculate_cash_on_cash_roi(self):
         rp = RentalProperty(2000, 1000, 500, 200, 500, 200, 100, 50, 50, 50, 50, 50, 50, 50, 100, 200, 500, 100, 1000, 50000, 10000, 20000, 500)
@@ -12,6 +13,17 @@ class Test_Rental_Property(unittest.TestCase):
         expected_income = 3700
         actual_income = rp.calculate_total_monthly_income()
         self.assertEqual(expected_income, actual_income)
+
+    def test_calculate_total_monthly_expenses(self):
+        rp = RentalProperty(150, 100, 0, 0, 0, 0, 0, 0, 0, 100, 100, 100, 200, 860)
+        expected_expenses = 3050
+        actual_expenses = rp.calculate_total_monthly_expenses()
+        self.assertEqual(expected_expenses, actual_expenses)
+
+    def test_calculate_total_monthly_cash_flow(self):
+        expected_cash_flow = (self.expected_income - self.expected_expenses)
+        self.assertEqual(expected_income = expected_cash_flow)
+        self.assertEqual(expected_expenses = expected_cash_flow)
 
 
 if __name__ == '__main__':
